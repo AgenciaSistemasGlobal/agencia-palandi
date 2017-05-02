bfdced6a955a0d628451f94066290dca36d7f38d
MuriloEduardo <muriloeduardoooooo@gmail.com>
2017-05-02 09:00:11 -0300

    Layout finalizado

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var porta = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(porta, function(){
-	console.log('Rodando na porta ' + porta);
-});
